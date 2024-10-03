9fa19cb99008c8ac50c01ac968f053cf2f5a7c32
Alice Wilson <alice.g.wilson@hotmail.co.uk>
2024-10-03 14:26:45 +0100

    add firebase credentials to .env

--- firebase.js
+++ firebase.js
@@ -3,16 +3,17 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAAYmKVvn-3kJYjBh5XI_Gb0KUkwhsjxjo",
-  authDomain: "sixty-escape-game.firebaseapp.com",
-  projectId: "sixty-escape-game",
-  storageBucket: "sixty-escape-game.appspot.com",
-  messagingSenderId: "559994329179",
-  appId: "1:559994329179:web:48b0fd20d57f877a15077e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+
 
 export { db, auth };
